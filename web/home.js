f877ff4a12896ae31473ea3d7cdd92fc12285e1d
S1EVEN0 <Stevenalejandro2020@gmail.com>
2026-08-03 11:30:16 -0500

    Novedad: Elimine botón de evento
    Removed event listener for 'IniciarSesion' button.

--- web/home.js
+++ web/home.js
@@ -1,6 +1,4 @@
-document.getElementById("IniciarSesion").addEventListener("click", function() {
-  window.location.href = "../login/login.html";
-});
+
 document.getElementById("Registrarse").addEventListener("click", function() {
   window.location.href = "../register/register.html";
 });
