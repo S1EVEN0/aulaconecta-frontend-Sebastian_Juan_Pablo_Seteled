044a90471c6d240f179ec11c4e19f6cb040a0950
juanpmar78 <juanpablomar78@gmail.com>
2026-08-03 11:07:07 -0500

    Creamos y agtregamos codigo a home.js

---
+++ web/home.js
@@ -0,0 +1,6 @@
+document.getElementById("IniciarSesion").addEventListener("click", function() {
+  window.location.href = "../login/login.html";
+});
+document.getElementById("Registrarse").addEventListener("click", function() {
+  window.location.href = "../register/register.html";
+});
